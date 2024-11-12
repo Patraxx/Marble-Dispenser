dc218bf567f9f523faba0e0609cbf21b3d26a702
Patraxx <patrik.cederlund@gmail.com>
2024-11-12 11:36:16 +0100

    main

--- main/main.c
+++ main/main.c
@@ -5,7 +5,14 @@
 
 //23 = 2nd column, 21 == 1st column, 19 == 3rd column
 // 22 == 1st row, 20 == 2nd row, 18 == 3rd row, 15 == 4th row
+
+int row_pins[] = {22, 20, 18, 15}; // Based on keypad's documentation for row mapping
+int col_pins[] = {21, 23, 19};
+
 void app_main(void)
 {
+
+
+    
 
 }
