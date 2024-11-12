08b442647b220b2c3a486847c7d2913a412f4cd7
Patraxx <patrik.cederlund@gmail.com>
2024-11-12 11:36:08 +0100

    first commit

---
+++ main/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+
+//23 = 2nd column, 21 == 1st column, 19 == 3rd column
+// 22 == 1st row, 20 == 2nd row, 18 == 3rd row, 15 == 4th row
+void app_main(void)
+{
+
+}
