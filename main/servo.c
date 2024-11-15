8732fda12905974d10b00a689c60aea79d79f55e
Patraxx <patrik.cederlund@gmail.com>
2024-11-15 10:46:08 +0100

    servo_test

--- main/servo.c
+++ main/servo.c
@@ -26,29 +26,3 @@ void initialize_servo()
     ledc_channel_config(&ledc_channel);
 }
 
-
-
-void move_servo(int speed)    // ellan 1 till 100 och -1 och -100
-{    
-    if (speed > 100) speed = 100;
-    if (speed < -100) speed = -100;
-    // Calculate the duty cycle
-
-    uint32_t pulsewidth = SERVO_NEUTRAL_PULSEWIDTH + (speed * (SERVO_MAX_PULSEWIDTH - SERVO_MIN_PULSEWIDTH) / 200);
-
-    // Scale the pulse width to the timer resolution
-    uint32_t duty = (pulsewidth * 2047) / 20000; 
-
-    printf("Speed: %d, Pulsewidth: %lu, Duty: %lu\n", speed, pulsewidth, duty);
-
-    ledc_set_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0, duty);
-    ledc_update_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0);
-
-    // Move the servo for about half a second
-    vTaskDelay(500 / portTICK_PERIOD_MS);
-
-    // Stop the servo by setting it to the neutral position
-    ledc_set_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0, (SERVO_NEUTRAL_PULSEWIDTH * 2047) / 20000);
-    ledc_update_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0);
-    
-}
