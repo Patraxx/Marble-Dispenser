d48489b8d5a4f8e61c13bdcbb7a3698a30a38382
Patraxx <patrik.cederlund@gmail.com>
2024-11-16 22:24:22 +0100

    hjj

--- main/main.c
+++ main/main.c
@@ -6,7 +6,6 @@
 
 void initialize_buttons(void)
 {
-   
     gpio_config_t config;
     config.pin_bit_mask = (1 << 10) | (1 << 11);
     config.mode = GPIO_MODE_INPUT;
@@ -14,16 +13,14 @@ void initialize_buttons(void)
     config.pull_down_en = GPIO_PULLDOWN_DISABLE;
     config.intr_type = GPIO_INTR_DISABLE;
     gpio_config(&config);
-
 }
 void move_servo(int speed)    // ellan 1 till 100 och -1 och -100
-{    
+{   
+     
     if (speed > 100) speed = 100;
     if (speed < -100) speed = -100;
     // Calculate the duty cycle
-
     uint32_t pulsewidth = SERVO_NEUTRAL_PULSEWIDTH + (speed * (SERVO_MAX_PULSEWIDTH - SERVO_MIN_PULSEWIDTH) / 200);
-
     // Scale the pulse width to the timer resolution
     uint32_t duty = (pulsewidth * 2047) / 20000; 
 
@@ -31,7 +28,6 @@ void move_servo(int speed)    // ellan 1 till 100 och -1 och -100
 
     ledc_set_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0, duty);
     ledc_update_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0);
-
     // Move the servo for about half a second
     vTaskDelay(500 / portTICK_PERIOD_MS);
 
