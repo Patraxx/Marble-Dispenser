1bb02ffbda911730e3b370fead0feb7fb75df45e
Patraxx <patrik.cederlund@gmail.com>
2024-11-18 08:35:29 +0100

    HF

--- main/servo.h
+++ main/servo.h
@@ -5,7 +5,7 @@
 #include "driver/ledc.h"
 
 #define SERVO_PIN 13 // GPIO pin connected to the servo
-#define SERVO_MAX_DUTY ((1 << LEDC_TIMER_16_BIT) - 1)
+
 #define SERVO_NEUTRAL_PULSEWIDTH 1500 // Neutral pulse width in microseconds
 #define SERVO_MIN_PULSEWIDTH 1000 // Minimum pulse width in microseconds
 #define SERVO_MAX_PULSEWIDTH 2000 
